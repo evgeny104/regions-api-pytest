3594b51bdf138d641615f5a26cf0bc35805db0c5
evgeny104 <nazawka88@gmail.com>
2026-07-10 18:18:02 +0300

    Fixed a bug in __repr__

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 
+DEFAULT_TIMEOUT = 15
+
+
 class Region:
     def __init__(self, url=None, q=None, country_code=None, page_size=None, page=None):
         self.url = url
@@ -12,21 +15,21 @@ class Region:
 
     def get_params(self):
         params = {}
-        if self.q:
+        if self.q is not None:
             params['q'] = self.q
-        if self.country_code:
+        if self.country_code is not None:
             params['country_code'] = self.country_code
-        if self.page_size:
+        if self.page_size is not None:
             params['page_size'] = self.page_size
-        if self.page:
+        if self.page is not None:
             params['page'] = self.page
         return params
 
-    def get_data(self):
-        response = requests.get(self.url, params=self.get_params())
+    def get_data(self, timeout=DEFAULT_TIMEOUT):
+        response = requests.get(self.url, params=self.get_params(), timeout=timeout)
         self.status_code = response.status_code
         self.data = response.json()
         return self.status_code, self.data
 
     def __repr__(self):
-        return self.status_code, self.data
+        return f"Region(status_code={self.status_code}, data={self.data})"
