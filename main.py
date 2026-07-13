0364fdc32a3b29d4a73480c3ebd33aa7475ef135
evgeny104 <nazawka88@gmail.com>
2026-07-13 23:10:17 +0300

    Reorder Region params: page before page_size

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ DEFAULT_TIMEOUT = 15
 
 
 class Region:
-    def __init__(self, url=None, q=None, country_code=None, page_size=None, page=None):
+    def __init__(self, url=None, q=None, country_code=None, page=None, page_size=None):
         self.url = url
         self.q = q
         self.country_code = country_code
-        self.page_size = page_size
         self.page = page
+        self.page_size = page_size
         self.data = None
         self.status_code = None
 
@@ -19,16 +19,20 @@ class Region:
             params['q'] = self.q
         if self.country_code is not None:
             params['country_code'] = self.country_code
-        if self.page_size is not None:
-            params['page_size'] = self.page_size
         if self.page is not None:
             params['page'] = self.page
+        if self.page_size is not None:
+            params['page_size'] = self.page_size
         return params
 
     def get_data(self, timeout=DEFAULT_TIMEOUT):
         response = requests.get(self.url, params=self.get_params(), timeout=timeout)
         self.status_code = response.status_code
-        self.data = response.json()
+        try:
+            self.data = response.json()
+        except ValueError:
+            self.data = {"error": {"id": "server_invalid_json",
+                                   "message": response.text[:500]}}
         return self.status_code, self.data
 
     def __repr__(self):
